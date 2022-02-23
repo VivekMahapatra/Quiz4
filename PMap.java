45a3759b7a733ed55de1accb39b242eb9a5b8fa7
VivekMahapatra <61105157+VivekMahapatra@users.noreply.github.com>
2022-02-23 12:54:27 -0600

    Add files via upload

---
+++ PMap.java
@@ -0,0 +1,189 @@
+/*
+ * Phase A <studentA EID><studentB EID>
+ * Phase B <studentB EID><studentA EID>
+ */
+
+package pmap.phasea;
+
+import java.util.ArrayList;
+
+/**
+ * PMap stands for Paired Map. A map is a collection of key-value
+ * pairs, e.g., (1, 2) (2, 3) (3, 4) are all pairs with an integer key
+ * and an integer value. See the
+ * <a href="https://docs.oracle.com/javase/8/docs/api/java/util/Map.html">
+ * java.util.Map documentation</a> for how these methods are supposed
+ * to work.
+ */
+public class PMap {
+	
+	private ArrayList<Integer> key;
+	private ArrayList<Integer> value;
+	
+	public PMap(){
+		key = new ArrayList<Integer>();
+		value = new ArrayList<Integer>();
+		
+	}
+
+    /**
+     * Returns the number of key-value mappings in this map.
+     *
+     * @return the number of key-value mappings in this map
+     */
+    public Integer size() {
+        return key.size();
+    }
+
+    /**
+     * Returns <tt>true</tt> if this map contains no key-value
+     * mappings.
+     *
+     * @return <tt>true</tt> if this map contains no key-value
+     *         mappings
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * Returns <tt>true</tt> if this map contains a mapping for the
+     * specified key.
+     *
+     * @param key key whose presence in this map is to be tested
+     * @return <tt>true</tt> if this map contains a mapping for the
+     *         specified key
+     */
+    public boolean containsKey(Integer key) {
+        return this.key.contains(key);
+    }
+
+    /**
+     * Returns <tt>true</tt> if this map maps one or more keys to the
+     * specified value.
+     *
+     * @param value value whose presence in this map is to be tested
+     * @return <tt>true</tt> if this map maps one or more keys to the
+     *         specified value
+     */
+    public boolean containsValue(Integer value) {
+        
+        return this.value.contains(value);
+    }
+
+    /**
+     * Returns the value to which the specified key is mapped, or
+     * {@code null} if this map contains no mapping for the key.
+     *
+     * @param key the key whose associated value is to be returned
+     * @return the value to which the specified key is mapped, or
+     *         {@code null} if this map contains no mapping for the
+     *         key
+     */
+    public Integer get(Integer key) {
+        if(containsKey(key) == false)
+        	return null;
+        return value.get(this.key.indexOf(key));
+    }
+
+    /**
+     * Associates the specified value with the specified key in this
+     * map (optional operation).  If the map previously contained a
+     * mapping for the key, the old value is replaced by the specified
+     * value.
+     *
+     * @param key key with which the specified value is to be
+     *        associated
+     * @param value value to be associated with the specified key
+     * @return the previous value associated with <tt>key</tt>, or
+     *         <tt>null</tt> if there was no mapping for <tt>key</tt>.
+     */
+    public Integer put(Integer key, Integer value) {
+        if(!containsKey(key)) {
+        	this.key.add(key);
+        	this.value.add(value);
+        	return null;
+        }
+        int index = this.key.indexOf(key);
+        int oldValue = this.value.get(index);
+        this.value.set(index, value);
+        
+        	
+        return oldValue;
+    }
+
+    /**
+     * Removes the mapping for a key from this map if it is present
+     * (optional operation).
+     *
+     * <p>Returns the value to which this map previously associated
+     * the key, or <tt>null</tt> if the map contained no mapping for
+     * the key.
+     *
+     * <p>The map will not contain a mapping for the specified key
+     * once the call returns.
+     *
+     * @param key key whose mapping is to be removed from the map
+     * @return the previous value associated with <tt>key</tt>, or
+     *         <tt>null</tt> if there was no mapping for <tt>key</tt>.
+     */
+    public Integer remove(Integer key) {
+        if(!containsKey(key))
+        	return null;
+        int index = this.key.indexOf(key);
+        int oldVal = this.value.get(index);
+        this.key.remove(index);
+        value.remove(index);
+        return oldVal;
+    }
+
+    /**
+     * Put all of the mappings from a pair of arrays to this map. Each
+     * pair of elements sharing the same index in <tt>keys</tt> and
+     * <tt>values</tt> composes a mapping to be stored the map.
+     *
+     * @param keys the array of keys to be stored in this map
+     * @param values the array of values to be stored in this map
+     */
+    public void putAll(Integer[] keys, Integer[] values) {
+        // TODO
+    }
+
+    /**
+     * Removes all of the mappings from this map. The map will be
+     * empty after this call returns.
+     */
+    public void clear() {
+        // TODO
+    }
+
+    /**
+     * Returns an array of the keys contained in this map.
+     *
+     * @return an array of the keys contained in this map
+     */
+    public Integer[] keySet() {
+        // TODO
+        return null;
+    }
+
+    /**
+     * Returns an array of the values contained in this map.
+     *
+     * @return an array of the values contained in this map
+     */
+    public Integer[] values() {
+        // TODO
+        return null;
+    }
+
+    /**
+     * Returns an array of the mappings contained in this map.
+     *
+     * @return an array of the mappings contained in this map
+     */
+    public PEntry[] entrySet() {
+        // TODO
+        return null;
+    }
+}
